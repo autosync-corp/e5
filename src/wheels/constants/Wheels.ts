638671d5a7d7f31547e9a3661d38bde8fc477df8
MilanDroid <antonybrenescruz@gmail.com>
2025-11-17 16:06:07 -0600

    Removing figma folder

--- src/wheels/constants/Wheels.ts
+++ src/wheels/constants/Wheels.ts
@@ -8,9 +8,9 @@ export const TALLADEGA_PODIUM_BRONZE = "/assets/images/wheels/edd03fa4648dfa8d21
 export const TALLADEGA_GLOSS_BLACK = "/assets/images/wheels/afdec958cf713eb929ae510752b31594f43c1a4f.png";
 export const TALLADEGA_PODIUM_GOLD = "/assets/images/wheels/464d2ace3214351d17a75848ec4bddc985f8c836.png";
 export const TALLADEGA_EXO = "/assets/images/talladega-exo.png";
-export const TALLADEGA_SHOT_1 = "/assets/figma/ac4f9bca958678b8de35e44893e3d5a5f294ec8b.png";
-export const TALLADEGA_SHOT_2 = "/assets/figma/81b6e7a67f0ff714d31b243e6080853c26608bba.png";
-export const TALLADEGA_SHOT_3 = "/assets/figma/6a649ab233804998e9706b78af02bf981a2fa348.png";
+export const TALLADEGA_SHOT_1 = "/assets/images/wheels/ac4f9bca958678b8de35e44893e3d5a5f294ec8b.png";
+export const TALLADEGA_SHOT_2 = "/assets/images/wheels/81b6e7a67f0ff714d31b243e6080853c26608bba.png";
+export const TALLADEGA_SHOT_3 = "/assets/images/wheels/6a649ab233804998e9706b78af02bf981a2fa348.png";
 export const TALLADEGA_SHOT_4 = "/assets/images/wheels/e5-talladega-rear.png";
 
 export const TALLADEGA_WHEELS = [
@@ -90,7 +90,7 @@ export const DAYTONA_WHEELS_BANNERS = [
 ];
 
 export const SPEEDWAY_BRAND_NAME = "Speedway";
-export const SPEEDWAY_LOGO = "/assets/figma/5a059fba9b6f32612b072a121dc09766ef507b86.png";
+export const SPEEDWAY_LOGO = "/assets/images/wheels/5a059fba9b6f32612b072a121dc09766ef507b86.png";
 export const SPEEDWAY_HERO_IMAGE = "/assets/images/speedway/speedway_hero_image.png";
 export const SPEEDWAY_PRECISION_FITMENT_BANNER = "/assets/images/speedway/speedway_precision_fitment_banner.png";
 export const SPEEDWAY_LIGHTWEIGHT_DESIGN_BANNER = "/assets/images/speedway/speedway_lightweight_design_banner.png";
@@ -104,7 +104,7 @@ export const SPEEDWAY_DETAIL_2 = "/assets/images/speedway/speedway_detail_2.png"
 export const SPEEDWAY_DETAIL_3 = "/assets/images/speedway/speedway_detail_3.png";
 export const SPEEDWAY_DETAIL_4 = "/assets/images/speedway/speedway_detail_4.png";
 export const SPEEDWAY_TITANIUM_BRUSHED = "/assets/images/wheels/9b42939207b0a1af6d11b3b9a2835f0304c9cedb.png";
-export const SPEEDWAY_TITANIUM_BRUSHED_TINT_BEAUTY_LEFT = "/assets/figma/2cb17a45657fa348958b15419df6f796aa26a518.png";
+export const SPEEDWAY_TITANIUM_BRUSHED_TINT_BEAUTY_LEFT = "/assets/images/wheels/2cb17a45657fa348958b15419df6f796aa26a518.png";
 export const SPEEDWAY_BRONZE = "/assets/images/wheels/55e2dad1e72a82c3b811737504db985db4ec5353.png";
 export const SPEEDWAY_GLOSS_BLACK = "/assets/images/wheels/cd19461c451031cb11b5e748b6774ae80b22a516.png";
 export const SPEEDWAY_WHEELS = [
@@ -124,7 +124,7 @@ export const SPEEDWAY_WHEELS = [
 
 // Sebring Wheels
 export const SEBRING_BRAND_NAME = "Sebring";
-export const SEBRING_LOGO = "/assets/figma/90f6c4d4b52eb6c3ec25ae4927cf62a98d647c4d.png";
+export const SEBRING_LOGO = "/assets/images/wheels/90f6c4d4b52eb6c3ec25ae4927cf62a98d647c4d.png";
 export const SEBRING_HERO_IMAGE = "/assets/images/sebring/sebring_hero_image.jpg";
 export const SEBRING_PRECISION_FITMENT_BANNER = "/assets/images/sebring/sebring_precision_fitment_banner.webp";
 export const SEBRING_LIGHTWEIGHT_DESIGN_BANNER = "/assets/images/sebring/sebring_lightweight_design_banner.png";
@@ -157,7 +157,7 @@ export const SEBRING_WHEELS = [
 ];
 
 export const E5_FORGED_LOGO = "/assets/images/wheels/02d80e5501d4b44b3e61daf67192b4fe23242b09.png";
-export const FORM_FORGED_LOGO = "/assets/figma/48b34752d7bd939ae556a6e8de9fa0837077ec48.png";
+export const FORM_FORGED_LOGO = "/assets/images/wheels/48b34752d7bd939ae556a6e8de9fa0837077ec48.png";
 export const FORM_FORGED_LOGO_BLACK = "/assets/images/form-forged-logo-black.png";
 export const FORM_FORGED_GROUP = "/assets/images/form-forged-group.png";
 export const VPS_FORGED_LOGO = "/assets/images/vossen_forged_banner.webp";
@@ -180,32 +180,32 @@ export const WHEEL_GALLERY_BANNERS_BY_BRAND: Record<string, { image: string, fin
 
 export const PROCESS_DIAGRAM = "/assets/images/our_process.webp";
 export const FORM_FORGED_STUDIO_IMAGES = [
-    "/assets/figma/458e21a13d0a34ebec2c1204b9ea4c2f5d42abd9.png",
-    "/assets/figma/4d0ad0fe85ca7d7fa24831e9b729b074e8030f19.png",
-    "/assets/figma/0d02c960dafa4520b863b7a2d4306d37966ea5e4.png",
-    "/assets/figma/dac7e09a2b1293503ee3cc0a55abc6a9614fffe9.png",
-    "/assets/figma/fa42b9726ec39042f657fe05feb192329c886522.png",
-    "/assets/figma/98db048dcecd1622510f41b23f78243bfe17a526.png"
+    "/assets/images/wheels/458e21a13d0a34ebec2c1204b9ea4c2f5d42abd9.png",
+    "/assets/images/wheels/4d0ad0fe85ca7d7fa24831e9b729b074e8030f19.png",
+    "/assets/images/wheels/0d02c960dafa4520b863b7a2d4306d37966ea5e4.png",
+    "/assets/images/wheels/dac7e09a2b1293503ee3cc0a55abc6a9614fffe9.png",
+    "/assets/images/wheels/fa42b9726ec39042f657fe05feb192329c886522.png",
+    "/assets/images/wheels/98db048dcecd1622510f41b23f78243bfe17a526.png"
 ];
 
 // Shop Section - Bottom 3 finishes
 export const SHOP_FINISHES = [
     {
-        image: "/assets/figma/77a1e54cf299ca527e8fc5695731f7a6680822fe.png",
+        image: "/assets/images/wheels/77a1e54cf299ca527e8fc5695731f7a6680822fe.png",
         finish: "Gloss Black",
         availability: "AVAILABLE FOR C6_C7_C8",
         price: "STARTING AT $600 PER WHEEL",
         stock: "IN STOCK"
     },
     {
-        image: "/assets/figma/6633a5fa3fbf86ab9e4ebada3acfa83d7ae98d30.png",
+        image: "/assets/images/wheels/6633a5fa3fbf86ab9e4ebada3acfa83d7ae98d30.png",
         finish: "Bronze Brushed Tint",
         availability: "AVAILABLE FOR C6_C7_C8",
         price: "STARTING AT $600 PER WHEEL",
         stock: "IN STOCK"
     },
     {
-        image: "/assets/figma/55e2dad1e72a82c3b811737504db985db4ec5353.png",
+        image: "/assets/images/wheels/55e2dad1e72a82c3b811737504db985db4ec5353.png",
         finish: "Dark Bronze",
         availability: "AVAILABLE FOR C5_C6_C7_C8",
         price: "STARTING AT $600 PER WHEEL",
