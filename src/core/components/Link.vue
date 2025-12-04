284f4053423ddc4972b029d4da4d82ddfb7b7189
MilanDroid <antonybrenescruz@gmail.com>
2025-12-04 13:39:56 -0600

    Mobile optimization

---
+++ src/core/components/Link.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  link: String,
+})
+</script>
+
+<template>
+  <div class="flex mx-auto cursor-pointer">
+    <a :href="props.link" class="text-[12px] tracking-[3.96px] uppercase border-b border-e5-red pb-2">
+      <div class="hover:-translate-y-0.5">
+        <slot />
+      </div>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
